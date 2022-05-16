69c44f7db150f38e513f173b5e140e57e608f899
Andrew Martins <andrew_omartins@yahoo.com>
2022-05-15 23:40:45 -0400

    added fuctionality for each task that is delted i or added in the app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,33 @@ function App() {
   const [tasks, setTasks] = useState([])
   const [showAddTask, setShowAddTask] = useState(false)
 
+  const addTask = (task) => {
+    const id = uuidv4()
+    const newTask = { id, ...task}
+    setTasks([ ...tasks, newTask])
+    Swal.fire({
+      icon: 'success',
+      title: 'Hell Yeah',
+      text: 'You have successfully added a new task!'
+    })
+  } 
+
+  const delteTask = (id) => {
+    const delteTask = tasks.filter((task) => task.id !== id)
+    setTasks(delteTask)
+    Swal.fire ({
+      icon: 'success',
+      title: 'Oops...',
+      text: 'You have successfully delted a task'
+    })
+  }
+
+
+
+
+
+
+
   return (
     <>
       <div className="Container">
