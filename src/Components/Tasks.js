83e374610c24ddbcf31a205f13508090a781c881
Andrew Martins <andrew_omartins@yahoo.com>
2022-05-15 22:47:57 -0400

    added form validation for incomplete inputs

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,8 +1,35 @@
 import React, { useState } from react
+import Swal from "sweetalert2"
 
 const AddTask = ({onSave}) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if(!text && !day) {
+            Swal.fire({
+                icon: 'error',
+                title: "Oops...",
+                text: "Fill in your tas and date or close the form"
+            })
+        } else if(!text && date) {
+            Swal.fire({
+                icon: 'error',
+                title: "Oops...",
+                text: "Fill in your task"
+            })
+        }else if(text && !date) {
+            Swal.fire({
+                icon: 'error',
+                title: "Oops...",
+                text: "Fill in your date"
+            })
+        } else {
+            onSave(text, day)
+        }
+        setText('')
+        setDay('')
+    }
 
 return (
     <form className="add-form" onSubmit={onSubmit}>
@@ -37,21 +64,5 @@ return (
 
 export default AddTask
 
-// const AddTask = ({onSave}) => {
-//     const [text, setText] = useState('');
-//     const [day, setDay] = useState('');
-//     return (
-//         <form className="add-form" onSubmit={onSubmit}>
-//             <div className="form-control">
-//                 <label>Task</label>
-//                 <input type="text" placeholder="add task" value={text} onChange={(e) => setText(e.target.value)} />
-//             </div>
-//             <div className="form-control">
-//                 <label>Day & Time</label>
-//                 <input type="text" placeholder="add day & time" value={day} onChange{(e) => setDay(e.target.value)} />
-//             </div>
-//             <input type="submit" className="btn btn-block" value="Save Task" />
-//         </form>
-//       )
-//   }
+
   
