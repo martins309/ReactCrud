9b85f11a41add42afe87672777ab2eecd6b01d3e
Andrew Martins <andrew_omartins@yahoo.com>
2022-05-16 23:36:50 -0400

    Added a spinner as a place holder as the data loads for practice

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import Swal from 'sweetalert2'
 function App() {
   const [tasks, setTasks] = useState([])
   const [showAddTask, setShowAddTask] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 3500)
+  }, [])
 
   const addTask = (task) => {
     const id = uuidv4()
@@ -38,16 +46,41 @@ function App() {
 
   return (
     <>
-      <div className="Container">
-        <Header showForm={() => setShowAddTask(!showAddTask)} changeText={showAddTask}/>
-        {showAddTask && <AddTask onSave={AddTask} />}
-
         {
-          tasks.length > 0 ?
-          (<Tasks tasks={tasks} />) :
-          ('No Task Found!')
+        loading ?
+          <div className="spinnerContainer">
+            <div className="spinner-grow text-primary" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-secondary" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-success" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+              <div className="spinner-grow text-danger" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+              <div className="spinner-grow text-warning" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+          </div> :
+          <div className="container">
+            {/* App Header that has open and App Name */}
+            <Header showForm={() => setShowAddTask(!showAddTask)} changeTextAndColor={showAddTask} />
+              {/* Revealing of Add Task Form */}
+              {showAddTask && <AddTask onSave={addTask} />}
+              {/* Task Counter */}
+              <h3>Number of Tasks: {tasks.length}</h3>
+              
+              {/* Displaying of Tasks */}
+              
+                tasks.length > 0 ?
+                  (<Tasks tasks={tasks} onDelete={deleteTask} onEdit={editTask} />) :
+                  ('No Task Found!')
+              }
+          </div>
         }
-      </div>
     </>
   );
 }
