3d8ccf9db801b12452674c3b52d5e31916726bd6
Andrew Martins <andrew_omartins@yahoo.com>
2022-05-21 21:43:33 -0400

    learn more with each iteration

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -1,68 +1,135 @@
 import React, { useState } from 'react'
-import Swal from "sweetalert2"
+import Swal from 'sweetalert2'
+
 
 const AddTask = ({onSave}) => {
-    const [text, setText] = useState('')
-    const [day, setDay] = useState('')
-    const onSubmit = (e) => {
-        e.preventDefault()
-        if(!text && !day) {
-            Swal.fire({
-                icon: 'error',
-                title: "Oops...",
-                text: "Fill in your tas and date or close the form"
-            })
-        } else if(!text && day) {
-            Swal.fire({
-                icon: 'error',
-                title: "Oops...",
-                text: "Fill in your task"
-            })
-        }else if(text && !day) {
-            Swal.fire({
-                icon: 'error',
-                title: "Oops...",
-                text: "Fill in your date"
-            })
-        } else {
-            onSave(text, day)
-        }
-        setText('')
-        setDay('')
-    }
+const [text,setText] = useState('')
+const [day, setDay] = useState('')
 
 return (
-    <form className="add-form" onSubmit={onSubmit}>
-        <div className="form-control">
+    <form className='add-form' onSubmit={onSubmit}>
+        <div className='form-control'>
             <label>Task</label>
-            <input
-            type= "text"
-            placeholder="add task"
-            value={text}
-            onChange= {(e) => setText(e.target.value)}
-            />
-        </div>
-        <div className="form-conrtrol">
-            <label>Day & Time</label>
-            <input
-            type = "text"
-            placeholder="add day and time"
+            <input 
+            type='text' 
+            placeholder='please enter a task' 
             value={text}
             onChange={(e) => setText(e.target.value)}
             />
         </div>
-        <input
-        type="submit"
-        className="btn btn-block"
-        value="Save Task"
-        />
+        <div>
+            <lable>Day and TIme</lable>
+            <input
+            tyoe='text'
+            placeholder='enter day and time'
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+            />
+        </div>
     </form>
 )
-
-
 }
 
-export default AddTask
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react'
+// import Swal from "sweetalert2"
+
+// const AddTask = ({onSave}) => {
+//     const [text, setText] = useState('')
+//     const [day, setDay] = useState('')
+//     const onSubmit = (e) => {
+//         e.preventDefault()
+//         if(!text && !day) {
+//             Swal.fire({
+//                 icon: 'error',
+//                 title: "Oops...",
+//                 text: "Fill in your tas and date or close the form"
+//             })
+//         } else if(!text && day) {
+//             Swal.fire({
+//                 icon: 'error',
+//                 title: "Oops...",
+//                 text: "Fill in your task"
+//             })
+//         }else if(text && !day) {
+//             Swal.fire({
+//                 icon: 'error',
+//                 title: "Oops...",
+//                 text: "Fill in your date"
+//             })
+//         } else {
+//             onSave(text, day)
+//         }
+//         setText('')
+//         setDay('')
+//     }
+
+// return (
+//     <form className="add-form" onSubmit={onSubmit}>
+//         <div className="form-control">
+//             <label>Task</label>
+//             <input
+//             type= "text"
+//             placeholder="add task"
+//             value={text}
+//             onChange= {(e) => setText(e.target.value)}
+//             />
+//         </div>
+//         <div className="form-conrtrol">
+//             <label>Day & Time</label>
+//             <input
+//             type = "text"
+//             placeholder="add day and time"
+//             value={text}
+//             onChange={(e) => setText(e.target.value)}
+//             />
+//         </div>
+//         <input
+//         type="submit"
+//         className="btn btn-block"
+//         value="Save Task"
+//         />
+//     </form>
+// )
+
+
+// }
+
+// export default AddTask
 
 
   
