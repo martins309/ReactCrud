ce0660e26a8e3fd41ee99336871527384cd5b453
Andrew Martins <andrew_omartins@yahoo.com>
2022-05-21 21:59:08 -0400

    wrote the addTaskk component

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -6,6 +6,37 @@ const AddTask = ({onSave}) => {
 const [text,setText] = useState('')
 const [day, setDay] = useState('')
 
+const onSubmit = (e) => {
+    e.preventDefault()
+    if(!day && !text) {
+        Swal.fire ({
+            icon: "error",
+            title: 'oops',
+            text: "Fill in your task and date"
+        })
+    } else if(!day && text) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops',
+            text: "fill in your date"
+        })
+    }else if(day && !text) {
+        Swal.fire({
+            icon: 'error',
+            title: "Oops",
+            text: 'Please enter a task'
+        })
+    }else {
+        onSave({ text, day })
+    }
+    setText('')
+    setDay('')
+}
+
+
+
+
+
 return (
     <form className='add-form' onSubmit={onSubmit}>
         <div className='form-control'>
@@ -26,6 +57,7 @@ return (
             onChange={(e) => setDay(e.target.value)}
             />
         </div>
+        <input type="submit" className='btn btn-block' value="Save Task"/>
     </form>
 )
 }
@@ -35,6 +67,7 @@ return (
 
 
 
+export default AddTask
 
 
 
@@ -45,91 +78,6 @@ return (
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react'
-// import Swal from "sweetalert2"
-
-// const AddTask = ({onSave}) => {
-//     const [text, setText] = useState('')
-//     const [day, setDay] = useState('')
-//     const onSubmit = (e) => {
-//         e.preventDefault()
-//         if(!text && !day) {
-//             Swal.fire({
-//                 icon: 'error',
-//                 title: "Oops...",
-//                 text: "Fill in your tas and date or close the form"
-//             })
-//         } else if(!text && day) {
-//             Swal.fire({
-//                 icon: 'error',
-//                 title: "Oops...",
-//                 text: "Fill in your task"
-//             })
-//         }else if(text && !day) {
-//             Swal.fire({
-//                 icon: 'error',
-//                 title: "Oops...",
-//                 text: "Fill in your date"
-//             })
-//         } else {
-//             onSave(text, day)
-//         }
-//         setText('')
-//         setDay('')
-//     }
-
-// return (
-//     <form className="add-form" onSubmit={onSubmit}>
-//         <div className="form-control">
-//             <label>Task</label>
-//             <input
-//             type= "text"
-//             placeholder="add task"
-//             value={text}
-//             onChange= {(e) => setText(e.target.value)}
-//             />
-//         </div>
-//         <div className="form-conrtrol">
-//             <label>Day & Time</label>
-//             <input
-//             type = "text"
-//             placeholder="add day and time"
-//             value={text}
-//             onChange={(e) => setText(e.target.value)}
-//             />
-//         </div>
-//         <input
-//         type="submit"
-//         className="btn btn-block"
-//         value="Save Task"
-//         />
-//     </form>
-// )
-
-
-// }
-
-// export default AddTask
 
 
   
